b91b3576c298688cd4baaf8d8070170f50b6a34a
Nidhi11-99 <dhimanndh@gmail.com>
2025-02-23 15:46:29 +0530

    added

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,24 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './layout/Header';
+import Home from './component/Home';
+import About from './component/About';
+import Contact from './component/Contact';
+import Categories from './component/Categories';
+import Register from './component/Register';
+import CustomerLogin from './component/CustomerLogin';
+import SellerLogin from './component/SellerLogin';
+import Blog from './component/Blog';
+import CustomerRegistration from './component/CustomerRegistration';
+import SellerRegistration from './component/SellerRegistration';
+import Wishlist from './component/Wishlist';
+import AddProduct from './component/AddProduct';
+import SellerDashboard from './component/SellerDashboard';
+import { BrowserRouter } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import { Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+   <BrowserRouter>
+   <Routes>
+    <Route path='/' element={<Home/>}></Route>
+    <Route path='/about' element={<About/>}></Route>
+    <Route path='/contact' element={<Contact/>}></Route>
+    <Route path='/categories' element={<Categories/>}></Route>
+    <Route path='/register' element={<Register/>}></Route>
+    <Route path='/login-customer' element={<CustomerLogin/>}></Route>
+    <Route path='/login-seller' element={<SellerLogin/>}></Route>
+    <Route path='/register-customer' element={<CustomerRegistration/>}></Route>
+    <Route path='/register-seller' element={<SellerRegistration/>}></Route>
+    <Route path='/blogs' element={<Blog/>}></Route>
+    <Route path="/seller-dashboard" element={<SellerDashboard />} />
+    <Route path='/add-product' element={<AddProduct/>}></Route>
+    <Route path='/wishlist' element={<Wishlist/>}></Route>
+
+
+   </Routes>
+   </BrowserRouter>
   );
 }
 
